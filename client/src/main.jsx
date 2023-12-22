c2a16ed8ad61c8413b02c49147c250fea520778b
pawansingh9305 <singhpawan.me@gmail.com>
2023-12-22 17:00:47 +0530

    Initial Commit

---
+++ client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+     domain="dev-ynppwct4qgfkq8wu.us.auth0.com"
+     clientId="oSkX76eWaSFRx2geTH8KgzKB9yNQ9L6a"
+     authorizationParams={{
+      redirect_uri: "http://127.0.0.1:5174"
+     }}
+     audience="http://localhost:8000"
+     scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
